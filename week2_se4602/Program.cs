4785206b5cad801011ea05d36f94ae7f363b970c
Fazlurraa21 <fazlur.r.n20@gmail.com>
2024-02-28 14:23:48 +0700

    First Commit

---
+++ week2_se4602/Program.cs
@@ -0,0 +1,9 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Input Nama :");
+        string nama = Console.ReadLine();
+        Console.WriteLine("Hello,  " + nama);
+    }
+}
